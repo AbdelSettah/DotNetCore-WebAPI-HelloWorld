df6bf70f65bae8d16a1c0eafec4c2969442df825
DESKTOP-46N3KU0\Abdel <settah43@yahoo.fr>
2020-11-27 19:10:08 +0100

    initial commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace scheduleApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public String hello()
+        {
+            return "hello world";
+        }
+    }
+}
